68bbe40d86915159758a8915a4fadf1c98e27ee8
Aryan Wadhwani <wadhwani@purdue.edu>
2021-11-10 13:03:45 -0500

    Work on Style Transfer

--- Lab3.py
+++ Lab3.py
@@ -1,15 +1,17 @@
 import os
+
+import PIL.Image
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow.keras.backend as K
 import random
 from scipy.optimize import fmin_l_bfgs_b
-from imageio import imread, imwrite
 from PIL import Image
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fmin_l_bfgs_b.html
 from tensorflow.keras.applications import vgg19
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import load_img, save_img
+from tensorflow.python.framework.ops import disable_eager_execution
 import warnings
 
 random.seed(1618)
@@ -21,6 +23,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # TODO: Take something cooler
 CONTENT_IMG_PATH = "inputImages/seashore.jpg"
 STYLE_IMG_PATH = "styleImages/vangogh.jpg"
+OUTPUT_IMG_PATH = "outputImages/output.jpg"
 
 CONTENT_IMG_H = 500
 CONTENT_IMG_W = 500
@@ -41,10 +44,14 @@ This function should take the tensor and re-convert it to an image.
 '''
 
 
-def deprocessImage(tsr):
-    tsr = tsr * 255
-    tsr = np.array(tsr, dtype=np.uint8)
-    img = tsr
+# https://keras.io/examples/generative/neural_style_transfer/
+def deprocessImage(img):
+    img = img.reshape((CONTENT_IMG_H, CONTENT_IMG_W, 3))
+    img[:, :, 0] += 103.939
+    img[:, :, 1] += 116.779
+    img[:, :, 2] += 123.68
+    img = img[:, :, ::-1]
+    img = np.clip(img, 0, 255).astype("uint8")
     return img
 
 
@@ -57,15 +64,37 @@ def gramMatrix(x):
 # ========================<Loss Function Builder Functions>======================
 
 def styleLoss(style, gen):
-    return None  # TODO: implement.
+    numFilters = 3
+    return K.sum(K.square(gramMatrix(style) - gramMatrix(gen))) / (
+            4. * (numFilters ^ 2) * ((CONTENT_IMG_H * CONTENT_IMG_W) ^ 2))
 
 
 def contentLoss(content, gen):
     return K.sum(K.square(gen - content))
 
 
+# https://keras.io/examples/generative/neural_style_transfer/
 def totalLoss(x):
-    return None  # TODO: implement.
+    a = tf.square(
+        x[:, : CONTENT_IMG_H - 1, : CONTENT_IMG_W - 1, :] -
+        x[:, 1:, : CONTENT_IMG_W - 1, :]
+    )
+    b = tf.square(
+        x[:, : CONTENT_IMG_H - 1, : CONTENT_IMG_W - 1, :] -
+        x[:, : CONTENT_IMG_H - 1, 1:, :]
+    )
+    return tf.reduce_sum(tf.pow(a + b, 1.25))
+
+
+def kAndFlatten(func):
+    def F(x):
+        x = x.reshape((1, CONTENT_IMG_H, CONTENT_IMG_W, 3))
+        ret = func([x])
+        loss = ret[0]
+        grad = ret[0].flatten().astype('float64')
+        return loss, grad
+
+    return F
 
 
 # =========================<Pipeline Functions>==================================
@@ -79,7 +108,7 @@ def getRawData():
     sImg = load_img(STYLE_IMG_PATH)
     print("      Images have been loaded.")
     return (
-    (cImg, CONTENT_IMG_H, CONTENT_IMG_W), (sImg, STYLE_IMG_H, STYLE_IMG_W), (tImg, CONTENT_IMG_H, CONTENT_IMG_W))
+        (cImg, CONTENT_IMG_H, CONTENT_IMG_W), (sImg, STYLE_IMG_H, STYLE_IMG_W), (tImg, CONTENT_IMG_H, CONTENT_IMG_W))
 
 
 def preprocessData(raw):
@@ -104,35 +133,59 @@ Save the newly generated and deprocessed images.
 
 
 def styleTransfer(cData, sData, tData):
+    disable_eager_execution()  # K.gradients is not supported  unless disabled
+
     print("   Building transfer model.")
     contentTensor = K.variable(cData)
     styleTensor = K.variable(sData)
     genTensor = K.placeholder((1, CONTENT_IMG_H, CONTENT_IMG_W, 3))
+
     inputTensor = K.concatenate([contentTensor, styleTensor, genTensor], axis=0)
-    model = None  # TODO: implement.
+    model = vgg19.VGG19(include_top=False, weights='imagenet', input_tensor=inputTensor)
     outputDict = dict([(layer.name, layer.output) for layer in model.layers])
+
     print("   VGG19 model loaded.")
     loss = 0.0
     styleLayerNames = ["block1_conv1", "block2_conv1", "block3_conv1", "block4_conv1", "block5_conv1"]
     contentLayerName = "block5_conv2"
+
     print("   Calculating content loss.")
     contentLayer = outputDict[contentLayerName]
     contentOutput = contentLayer[0, :, :, :]
     genOutput = contentLayer[2, :, :, :]
-    loss += None  # TODO: implement.
+    loss += CONTENT_WEIGHT * contentLoss(contentOutput, genOutput)
+
     print("   Calculating style loss.")
     for layerName in styleLayerNames:
-        loss += None  # TODO: implement.
-    loss += None  # TODO: implement.
+        layer_f = outputDict[layerName]
+        style_f = layer_f[1, :, :, :]
+        out_f = layer_f[2, :, :, :]
+        loss += (STYLE_WEIGHT / len(styleLayerNames)) * styleLoss(style_f, out_f)
+    print("   Calculating total var loss")
+    loss += TOTAL_WEIGHT * totalLoss(genTensor)
+
+    print("   Setting up Gradients")
     # TODO: Setup gradients or use K.gradients().
+    outputs = [loss]
+    grads = K.gradients(loss, genTensor)[0]
+    outputs += grads
+
+    k_f = kAndFlatten(K.function([genTensor], outputs))
+    gen = tData.flatten()  # Start with input image
+    print(gen.shape)
+
     print("   Beginning transfer.")
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
-        # TODO: perform gradient descent using fmin_l_bfgs_b.
-        # print("      Loss: %f." % tLoss)
-        # img = deprocessImage(x)
-        saveFile = None  # TODO: Implement.
-        # imsave(saveFile, img)   #Uncomment when everything is working right.
+        gen_new, gen_loss, _ = fmin_l_bfgs_b(func=k_f, x0=gen, maxiter=50)
+
+        gen = gen_new.copy()
+        print("      Loss: ", gen_loss)
+
+        img = deprocessImage(gen)
+        saveFile = f"outputImages/output_{i}.jpg"
+
+        save_img(saveFile, img)
         print("      Image saved to \"%s\"." % saveFile)
     print("   Transfer complete.")
 
@@ -145,7 +198,7 @@ def main():
     cData = preprocessData(raw[0])  # Content image.
     sData = preprocessData(raw[1])  # Style image.
     tData = preprocessData(raw[2])  # Transfer image.
-    # styleTransfer(cData, sData, tData)
+    styleTransfer(cData, sData, tData)
     print("Done. Goodbye.")
 
 
